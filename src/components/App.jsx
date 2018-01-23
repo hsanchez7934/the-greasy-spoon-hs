49d12abfc2bfa2e1b5972d979047ea67bbd050df
Hector Sanchez <hsanchez7934@gmail.com>
2018-01-23 16:54:48 -0700

    Setup redux, linter, add scripts to package.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import apiKey from '../apikey.js';
 
 class App extends Component {
+
+  componentDidMount() {
+    fetch(`https://check-api.herokuapp.com/tables`, {
+      headers: {
+        Authorization: apiKey
+      }
+    })
+    .then(response => response.json())
+    .then(response => console.log(response))
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        hello world
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App.jsx';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
