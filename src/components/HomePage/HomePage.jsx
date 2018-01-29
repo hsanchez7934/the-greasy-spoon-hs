e91be6e9436ee7f80ae4c8a4a8e5c84457496320
Hector Sanchez <hsanchez7934@gmail.com>
2018-01-29 12:00:02 -0700

    Clean up homepage component

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -8,11 +8,15 @@ const HomePage = () => (
       <h1 id='homepage-title'>The Greasy Spoon</h1>
       <h2 id='homepage-welcome'>Welcome</h2>
       <p id='homepage-text'>
-        We will be placing orders for our valued customer using this application.
-        The sidebar located on the left will help you to easily
-        navigate the app.  The Tables link will show you which tables are
-        are available to open a check against.  Click on the Open Checks and Closed Checks links
-        to view open and closed checks.  Happy serving!
+        We will be placing orders for our valued
+        customers using this application. The
+        sidebar located on the left will help
+        you to easily navigate the app.  The
+        Tables link will show you which tables
+        are are available to open a check against.
+        Click on the Open Checks and Closed Checks
+        links to view open and closed checks.
+        Happy serving!
       </p>
       <div id='get-started-button-container'>
         <Link to='/tables' id='get-started-button-link-tag'>
