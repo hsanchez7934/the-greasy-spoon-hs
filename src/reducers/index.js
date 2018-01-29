635e4157dacbaec56a62cc6c9d4e212313f80127
Hector Sanchez <hsanchez7934@gmail.com>
2018-01-29 12:20:59 -0700

    Deleted unused reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,19 +1,13 @@
 import { combineReducers } from 'redux';
 import tables from './tablesReducer.js';
 import checks from './checksReducer.js';
-import addedCheckReducer from './addCheckReducer.js';
 import items from './itemsReducer.js';
-import addItemToCheckReducer from './putItemReducer.js';
 import checkByIdReducer from './checkByIdReducer.js';
-import closedCheckReducer from './closedCheckReducer.js';
 
 const rootReducer = combineReducers({
   tables,
   items,
-  addedCheckReducer,
-  addItemToCheckReducer,
   checkByIdReducer,
-  closedCheckReducer,
   checks
 });
 
