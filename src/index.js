ea0de5c1eae7810d39453808c86c7838871bc3f7
Hector Sanchez <hsanchez7934@gmail.com>
2018-01-29 13:26:21 -0700

    Add .env file

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './components/App/App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore.js';
+require('dotenv').config();
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 const store = configureStore();
