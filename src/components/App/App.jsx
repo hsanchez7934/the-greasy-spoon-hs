cbd055b48c3f4592804955ff9a84653f5cd6068f
Hector Sanchez <hsanchez7934@gmail.com>
2018-01-29 11:31:48 -0700

    Write App.jsx into functionless component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,32 +4,10 @@ import apiKey from '../../apikey.js';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import HomeScreen from '../HomeScreen/HomeScreen.jsx';
 
-class App extends Component {
-
-  componentDidMount() {
-    this.cleanSlate();
-  }
-
-
-  cleanSlate = () => {
-    fetch(`https://check-api.herokuapp.com/checks`, {
-      method: 'DELETE',
-      headers: {
-        Authorization: apiKey
-      }
-    })
-      .then(response => response.json())
-      .then(response => response)
-      .catch(error => error);
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <HomeScreen />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <HomeScreen />
+  </div>
+);
 
 export default App;
