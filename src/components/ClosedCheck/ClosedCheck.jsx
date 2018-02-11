e0e2fc4ab99bb7bf03e83d5f3ac6afe3c6a2e3e4
Hector Sanchez <hsanchez7934@gmail.com>
2018-02-11 00:27:21 -0700

    Add condition for filter item function in closed check component

--- src/components/ClosedCheck/ClosedCheck.jsx
+++ src/components/ClosedCheck/ClosedCheck.jsx
@@ -6,9 +6,10 @@ import PropTypes from 'prop-types';
 export default class ClosedCheck extends Component {
 
   filterItem = (id) => {
-    const filteredItem = this.props.items.filter( item => item.id === id);
-    console.log(filteredItem);
-    return filteredItem[0];
+    if (this.props.items.length) {
+      const filteredItem = this.props.items.filter( item => item.id === id);
+      return filteredItem[0];
+    }
   };
 
   voidedClassName = (item) => {
@@ -65,56 +66,47 @@ export default class ClosedCheck extends Component {
   );
 
   render () {
-    console.log(this.props.items);
     const { storedCheck, table } = this.props;
-    if (!this.props.items) {
-      return (
-        <div>
-          no
-        </div>
-      )
-    } else {
-      return (
-        <article className='closedcheck-card' id={storedCheck.id}>
-          <h3 className='closed-title-table'>Table {table.number}</h3>
-          <h2 className='closed-check-title'>Closed Check</h2>
-          <section className='closed-ordered-items'>
-            <p className='oitems-title'>Ordered Items</p>
-            <ul className='closed-ordered-items-ul'>
-              {
-                this.createOrderedItems()
-              }
-            </ul>
-          </section>
-          <p className='tax-tip'>
-            Sub-Total: $
+    return (
+      <article className='closedcheck-card' id={storedCheck.id}>
+        <h3 className='closed-title-table'>Table {table.number}</h3>
+        <h2 className='closed-check-title'>Closed Check</h2>
+        <section className='closed-ordered-items'>
+          <p className='oitems-title'>Ordered Items</p>
+          <ul className='closed-ordered-items-ul'>
             {
-              this.itemsTotal().toFixed(2)
+              this.createOrderedItems()
             }
-          </p>
-          <p className='tax-tip'>
-            Tax: {storedCheck.tax}
-          </p>
-          <p className='tax-tip'>
-            Tip: {storedCheck.tip}
-          </p>
-          <p className='closed-customer-total'>
-            Total: ${this.finalTotal().toFixed(2)}
-          </p>
-          <div className='exit-check-button-container'>
-            <Link
-              to='/closedchecks'
-              className='exit-button-link'>
-              <button
-                className='exit-button'
-                onClick={() => this.props.newCheckAdded(true)}>
-                EXIT
-              </button>
-            </Link>
-          </div>
-        </article>
-      );
-    }
+          </ul>
+        </section>
+        <p className='tax-tip'>
+          Sub-Total: $
+          {
+            this.itemsTotal().toFixed(2)
+          }
+        </p>
+        <p className='tax-tip'>
+          Tax: {storedCheck.tax}
+        </p>
+        <p className='tax-tip'>
+          Tip: {storedCheck.tip}
+        </p>
+        <p className='closed-customer-total'>
+          Total: ${this.finalTotal().toFixed(2)}
+        </p>
+        <div className='exit-check-button-container'>
+          <Link
+            to='/closedchecks'
+            className='exit-button-link'>
+            <button
+              className='exit-button'
+              onClick={() => this.props.newCheckAdded(true)}>
+              EXIT
+            </button>
+          </Link>
+        </div>
+      </article>
+    );
   }
 }
 
