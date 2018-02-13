604f8092512a2c58d1935b7c98b242d03c1d4648
Hector Sanchez <hsanchez7934@gmail.com>
2018-02-12 22:56:55 -0700

    Write tests for App component, actions, and reducers

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { mount } from 'enzyme';
+import config from '../../tests/testSetup.js';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe(`App component unit test`, () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<App/>);
+  });
+
+  test(`should render an instance of App`, () => {
+    expect(wrapper.exists()).toEqual(true);
+  });
+
+  test('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
